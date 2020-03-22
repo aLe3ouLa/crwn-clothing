c01bf8f96ea692977bcdcedad1cee5dcdeeb6704
aLe3ouLa <ale3mp90@gmail.com>
2020-03-22 13:43:19 +0100

    Add sign in functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,16 +11,39 @@ const config = {
     messagingSenderId: "188956813914",
     appId: "1:188956813914:web:59c4a2cf46b4896c1c8663",
     measurementId: "G-DB3CYPS6ND"
-  };
+};
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+    return userRef;
+  };
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;

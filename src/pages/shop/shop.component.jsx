6466d895c0f84ff4cd20f0e735cc03f9d18d3672
aLe3ouLa <ale3mp90@gmail.com>
2020-03-27 09:40:02 +0100

    Move shop to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className = "shop-page">
-                {collections.map(({id , ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} /> 
-                )
-                )}
-            </div>
-        )
-    }
-}
+const  ShopPage = () => 
+    (<div className = "shop-page">
+        <CollectionOverview />
+    </div>);
 
 export default ShopPage;

fcd34047181080ab6e69dc35a57bba5c7921f4e1
aLe3ouLa <ale3mp90@gmail.com>
2020-03-29 16:26:02 +0200

    Translate components to styled components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,15 +5,15 @@ import { createStructuredSelector } from 'reselect';
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
+import { DirectoryContainer } from './directory.styles';
 
-import './directory.styles.scss';
 
 const Directory = ({ sections }) => (
-  <div className='directory-menu'>
+  <DirectoryContainer>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </div>
+  </DirectoryContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
